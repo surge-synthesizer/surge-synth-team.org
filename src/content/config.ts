b32e83ca71d7b92e2c87cc98426cd97d31692a2a
Mike Thierman <mthierman@gmail.com>
2024-03-22 18:24:02 -0600

    Routing (#99)
    * Re-do routing, simplify styling
    
    * Fix paragraph spacing
    
    * Backdrop blur in nav
    
    * Fix logo centering
    
    * Add underline offset and fix end paragraph margins
    
    * Fix image margin in project pages
    
    * Better image margin fix
    
    * Fix header centering
    
    * Final image margin fix
    
    * Fix header margins in index cards

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,26 @@ import { defineCollection, z } from "astro:content";
 
 const pages = defineCollection({
     type: "content",
+    schema: z.object({
+        title: z.string(),
+    }),
+});
+
+const projects = defineCollection({
+    type: "content",
     schema: ({ image }) =>
         z.object({
             title: z.string(),
             order: z.number(),
-            summary: z.string().optional(),
-            cover: image().optional(),
-            categories: z.array(z.string()).optional(),
-            url: z.string().optional(),
-            issue_tracker: z.string().optional(),
+            summary: z.string(),
+            cover: image(),
+            categories: z.string().array(),
+            url: z.string(),
+            issue_tracker: z.string(),
         }),
 });
 
 export const collections = {
     pages: pages,
+    projects: projects,
 };
