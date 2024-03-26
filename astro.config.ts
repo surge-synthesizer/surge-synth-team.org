745babb15cf6159145a5496e9e9bca36f2ef8c60
mthierman <mthierman@gmail.com>
2024-03-25 23:46:36 -0600

    Add dark/light mode toggle

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: "solarized-light",
+                dark: "solarized-dark",
+            },
+            wrap: true,
+        },
+    },
     integrations: [mdx(), react(), tailwind()],
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,4 +15,5 @@ export default {
             },
         },
     },
+    darkMode: "selector",
 } satisfies Config;
