4242d50f0b79753c98fa7d51fdf0d3fe11fab515
Mike Thierman <mthierman@gmail.com>
2025-01-26 22:46:44 -0700

    Migrate to Astro 5 and Tailwind 4 (#105)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,15 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const pages = defineCollection({
-    type: "content",
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/pages" }),
     schema: z.object({
         title: z.string(),
     }),
 });
 
 const projects = defineCollection({
-    type: "content",
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
     schema: ({ image }) =>
         z.object({
             title: z.string(),
