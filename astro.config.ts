b68cf4aa7737dc7b24f3dd54af093abc73d6f237
Mike Thierman <mthierman@gmail.com>
2025-10-08 09:49:16 -0600

    Styling overhaul (#117)

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,9 @@
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     vite: { plugins: [tailwindcss()] },
-    integrations: [mdx(), react()],
-    markdown: {
-        shikiConfig: {
-            themes: {
-                light: "dark-plus",
-                dark: "light-plus",
-            },
-            wrap: true,
-        },
-    },
+    integrations: [mdx()],
 });
