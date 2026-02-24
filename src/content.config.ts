e8145fe66937563b5535d1ea7a8324e1c525c452
Mike Thierman <mthierman@gmail.com>
2026-02-24 15:56:01 -0700

    OB-Xf: Use Web Component download button (#127)

--- src/content.config.ts
+++ src/content.config.ts
@@ -26,7 +26,32 @@ const projects = defineCollection({
         }),
 });
 
+const generated = defineCollection({
+    loader: glob({ pattern: "**/*.json", base: "./src/content/generated" }),
+    schema: () =>
+        z.object({
+            build_time: z.string(),
+            data: z.record(z.any()),
+            latest_commit: z.string(),
+            name: z.string(),
+            project_repo: z.string().url(),
+            recent_commits: z
+                .array(
+                    z.object({
+                        author: z.string(),
+                        commit: z.string(),
+                        date: z.string(),
+                        message: z.string(),
+                    }),
+                )
+                .optional(),
+            releases: z.record(z.string()),
+            version: z.string(),
+        }),
+});
+
 export const collections = {
     pages,
     projects,
+    generated,
 };
