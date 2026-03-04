a9b22ab200407c3fff031d6c71bdd21b76ef6377
mthierman <mthierman@gmail.com>
2026-03-03 20:00:13 -0700

    Fix ordering

--- astro.config.ts
+++ astro.config.ts
@@ -6,13 +6,36 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     vite: { plugins: [tailwindcss()] },
+    redirects: {
+        "/ob-xf/manual/": "/ob-xf/manual/getting-started/",
+    },
     integrations: [
         starlight({
             title: "OB-Xf Manual",
             sidebar: [
                 {
                     label: "OB-Xf Manual",
-                    autogenerate: { directory: "ob-xf/manual" },
+                    items: [
+                        { label: "Getting Started", slug: "ob-xf/manual/getting-started" },
+                        {
+                            label: "Installing or Building OB-Xf",
+                            slug: "ob-xf/manual/installing-or-building",
+                        },
+                        {
+                            label: "User Interface Basics",
+                            slug: "ob-xf/manual/user-interface-basics",
+                        },
+                        { label: "Patch Memory", slug: "ob-xf/manual/patch-memory" },
+                        { label: "Master", slug: "ob-xf/manual/master" },
+                        { label: "Global", slug: "ob-xf/manual/global" },
+                        { label: "Control", slug: "ob-xf/manual/control" },
+                        { label: "Oscilllators", slug: "ob-xf/manual/oscillators" },
+                        { label: "Mixer", slug: "ob-xf/manual/mixer" },
+                        { label: "Filter", slug: "ob-xf/manual/filter" },
+                        { label: "LFO", slug: "ob-xf/manual/lfo" },
+                        { label: "Envelopes", slug: "ob-xf/manual/envelopes" },
+                        { label: "Voice Variation", slug: "ob-xf/manual/voice-variation" },
+                    ],
                 },
             ],
         }),
